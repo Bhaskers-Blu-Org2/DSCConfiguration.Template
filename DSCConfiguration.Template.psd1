8232862ca324a6fe62adca81fffa2eeb698a6417
Michael Greene <michael.greene@microsoft.com>
2016-12-14 08:39:41 -0600

    reference in manifest name correction

--- DSCConfiguration.Template.psd1
+++ DSCConfiguration.Template.psd1
@@ -9,7 +9,7 @@
 @{
 
 # Script module or binary module file associated with this manifest.
-RootModule = '.\Template.Config.psm1'
+RootModule = '.\DSCConfiguration.Template.psm1'
 
 # Version number of this module.
 ModuleVersion = '0.1.0.0'
--- Tests/.Unit/DSCConfiguration.Template.Unit.Tests.ps1
+++ Tests/.Unit/DSCConfiguration.Template.Unit.Tests.ps1
@@ -1,1 +1,29 @@
-#
+<#
+#>
+
+# before:
+
+# Should have a psm1, psd1, and readme
+
+# psd1 should import
+
+# manifest should have name, id, root mod
+
+# requirements should be found in gallery
+
+# requirements should install locally
+
+# each configuration should compile locally
+
+# each configuration should produce a mof
+
+# after:
+# technically are these unit or integration?
+
+# modules should be in AADSC
+
+# modules should show extracted activities
+
+# configurations should be in AADSC
+
+# configurations should show as compiled
