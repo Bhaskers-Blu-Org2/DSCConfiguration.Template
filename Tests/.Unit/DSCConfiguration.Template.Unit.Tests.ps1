281589a7591bacf527bf0117c6e422f05bc86bca
Michael Greene <michael.greene@microsoft.com>
2016-12-14 08:46:39 -0600

    introduce unit tests

--- Tests/.Unit/DSCConfiguration.Template.Unit.Tests.ps1
+++ Tests/.Unit/DSCConfiguration.Template.Unit.Tests.ps1
@@ -1,9 +1,20 @@
 <#
 #>
 
-# before:
-
-# Should have a psm1, psd1, and readme
+Describe 'Universal configuration tests' {
+    Context 'Module properties' {
+        $Files = Get-ChildItem
+        It 'Contains a module file' {
+            $Files.Names.Contains("$env:APPVEYOR_PROJECT_NAME.psm1") | Should Be True
+        }
+        It 'Contains a module manifest' {
+            $Files.Names.Contains("$env:APPVEYOR_PROJECT_NAME.psd1") | Should Be True
+        }
+        It 'Contains a readme' {
+            $Files.Names.Contains('README.md') | Should Be True
+        }
+    }
+}
 
 # psd1 should import
 
