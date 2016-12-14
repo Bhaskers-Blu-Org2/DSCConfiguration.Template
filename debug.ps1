fa649483164a0be428347c128c8bc6e43efbd566
Michael Greene <michael.greene@microsoft.com>
2016-12-14 11:39:19 -0600

    fixed in test script

--- debug.ps1
+++
@@ -1,8 +0,0 @@
-$moduleRootFilePath = Split-Path -Path $PSScriptRoot -Parent
-$moduleRootFilePath
-$moduleName = (Get-Item -Path $moduleRootFilePath).Name
-$moduleName
-$moduleManifestPath = Join-Path -Path $moduleRootFilePath -ChildPath "$moduleName.psd1"
-$moduleManifestPath
-$moduleManifestProperties = Test-ModuleManifest -Path $moduleManifestPath -ErrorAction 'SilentlyContinue'
-$moduleManifestProperties.PowerShellVersion
